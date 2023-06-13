30e8e7c1fe965c6f41fc208a28158a8e725d3b29
zamzamnurahman <2006126itg.ac.id>
2023-06-13 09:47:26 +0700

    auth screen done navigate to home

--- lib/src/presentations/screens/home_screen.dart
+++ lib/src/presentations/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomodoro_task_manager/src/config/theme.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +13,40 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home screen"),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text("Pomodoro Task Manager",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: primary,
+        onTap: (index) {},
+        items: const [
+          BottomNavigationBarItem(
+            label: "Pomodoro",
+            icon: Icon(
+              Icons.timer,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Tugas",
+            icon: Icon(
+              Icons.task_alt,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: "Profil",
+            icon: Icon(
+              Icons.person,
+            ),
+          ),
+        ],
       ),
     );
   }
