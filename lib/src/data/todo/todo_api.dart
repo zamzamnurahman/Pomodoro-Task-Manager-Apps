b60cd9bcdaa6636c99db7f0f44ecfcdff616857e
zamzamnurahman <2006126itg.ac.id>
2023-06-28 23:57:44 +0700

    add cloud firestore update delete

--- lib/src/data/todo/todo_api.dart
+++ lib/src/data/todo/todo_api.dart
@@ -8,4 +8,18 @@ class TodoApi {
       "created_at": DateTime.now(),
     }).then((value) => print(value));
   }
+
+  Future<void> editProduct(bool status,String id) async {
+    await FirebaseFirestore.instance
+        .collection("todo")
+        .doc(id)
+        .update({"status": status});
+  }
+
+  Future<void> deleteProduct(DocumentSnapshot doc) async {
+    await FirebaseFirestore.instance
+        .collection("todo")
+        .doc(doc.id)
+        .delete();
+  }
 }
--- lib/src/presentations/screens/task_screens.dart
+++ lib/src/presentations/screens/task_screens.dart
@@ -1,24 +1,250 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomodoro_task_manager/src/data/todo/todo_api.dart';
 
-class TaskScreens extends ConsumerWidget {
+import '../../config/theme.dart';
+
+class TaskScreens extends ConsumerStatefulWidget {
   const TaskScreens({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        ElevatedButton.icon(
-          onPressed: () {
-            TodoApi().uploadingData(title: "testing #1");
-          },
-          label: Text("Tambah Todo"),
-          icon: Icon(Icons.add),
+  ConsumerState<ConsumerStatefulWidget> createState() => _TaskScreensState();
+}
+
+class _TaskScreensState extends ConsumerState<TaskScreens> {
+  final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
+  final PageController _pageController = PageController();
+  late TextEditingController _titleCtrl;
+  @override
+  void initState() {
+    _titleCtrl = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleCtrl.dispose();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: PageView(
+          controller: _pageController,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ListTile(
+                      title: const Text(
+                        "Tugas Belum Selesai",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      trailing: TextButton(
+                          onPressed: () {
+                            _pageController.animateToPage(
+                              1,
+                              duration: const Duration(milliseconds: 200),
+                              curve: Curves.bounceIn,
+                            );
+                          },
+                          child: const Text("Belum Selesai")),
+                    ),
+                    StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection("todo")
+                            .snapshots(),
+                        builder: (_, snapshot) {
+                          if (snapshot.hasData) {
+                            return Expanded(
+                              child: ListView.builder(
+                                  physics: const BouncingScrollPhysics(),
+                                  itemCount: snapshot.data!.docs.length,
+                                  itemBuilder: (_, index) {
+                                    final DocumentSnapshot todo =
+                                        snapshot.data!.docs[index];
+                                    if (!todo['status']) {
+                                      return Dismissible(
+                                        background: Container(
+                                          color: Colors.red,
+                                          child: const Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            children: [
+                                              Icon(Icons.clear_rounded),
+                                              Icon(Icons.clear_rounded),
+                                            ],
+                                          ),
+                                        ),
+                                        key: Key(todo.id),
+                                        onDismissed: (direction) {
+                                          TodoApi().deleteProduct(todo);
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(const SnackBar(
+                                                  behavior:
+                                                      SnackBarBehavior.floating,
+                                                  backgroundColor: Colors.red,
+                                                  content: Text(
+                                                      'Tugas berhasil dihapus')));
+                                        },
+                                        child: Card(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                          ),
+                                          child: ListTile(
+                                            title: Text(todo['title']),
+                                            trailing: IconButton(
+                                              onPressed: () {
+                                                TodoApi()
+                                                    .editProduct(true, todo.id);
+                                              },
+                                              icon: const Icon(Icons.check),
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    } else {
+                                      return const Visibility(
+                                          visible: false, child: SizedBox());
+                                    }
+                                  }),
+                            );
+                          } else {
+                            return const Center(
+                              child: Text("Tidak ada tugas"),
+                            );
+                          }
+                        }),
+                    Form(
+                      key: _globalKey,
+                      child: Container(
+                        margin: const EdgeInsets.all(10),
+                        height: 70,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: const Offset(0, -2),
+                                  blurRadius: 5,
+                                  spreadRadius: 2,
+                                  color: Colors.black.withOpacity(0.2))
+                            ]),
+                        alignment: Alignment.bottomCenter,
+                        child: TextFormField(
+                          controller: _titleCtrl,
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.all(20),
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                if (_globalKey.currentState!.validate()) {
+                                  TodoApi()
+                                      .uploadingData(title: _titleCtrl.text);
+                                  _titleCtrl.clear();
+                                }
+                                FocusScope.of(context).unfocus();
+                              },
+                              icon: const Icon(Icons.add),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Harap isi";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ListTile(
+                  title: GestureDetector(
+                      onTap: () {
+                        _pageController.animateToPage(0,
+                            duration: const Duration(milliseconds: 200),
+                            curve: Curves.bounceIn);
+                      },
+                      child: const Text(
+                        "Tugas Selesai",
+                        style: TextStyle(fontSize: 14),
+                      )),
+                  trailing: const Text(
+                    "Belum Selesai",
+                    style: TextStyle(
+                        color: primary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("todo")
+                        .snapshots(),
+                    builder: (_, snapshot) {
+                      if (snapshot.hasData) {
+                        return Expanded(
+                          child: ListView.builder(
+                              physics: const BouncingScrollPhysics(),
+                              itemCount: snapshot.data!.docs.length,
+                              itemBuilder: (_, index) {
+                                final DocumentSnapshot todo =
+                                    snapshot.data!.docs[index];
+                                if (todo['status']) {
+                                  return Card(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: ListTile(
+                                      title: Text(
+                                        todo['title'],
+                                        style: const TextStyle(
+                                            decoration:
+                                                TextDecoration.lineThrough),
+                                      ),
+                                      trailing: IconButton(
+                                        onPressed: () {
+                                          TodoApi().editProduct(false, todo.id);
+                                        },
+                                        icon: const Icon(Icons.close),
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return const Visibility(
+                                      visible: false, child: SizedBox());
+                                }
+                              }),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text("Tidak ada tugas"),
+                        );
+                      }
+                    }),
+              ],
+            ),
+          ],
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
