8d4fb261f57b29c40051227d4b7ddb7a6eace97d
zamzamnurahman <2006126itg.ac.id>
2023-06-28 14:25:11 +0700

    add pomodoro timer for 3 type

--- lib/src/presentations/screens/pomodoro_screen.dart
+++ lib/src/presentations/screens/pomodoro_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomodoro_task_manager/src/config/theme.dart';
 
+///[NAVIGATION MENU]
 final navProvider = StateNotifierProvider<NavNotifier, int>((ref) {
   return NavNotifier();
 });
@@ -11,6 +12,18 @@ class NavNotifier extends StateNotifier<int> {
   NavNotifier() : super(0);
 
   setNavigation(int newIndex) => state = newIndex;
+}
+
+///[ISPLAY TIMER]
+final playtimerProvider =
+    StateNotifierProvider<PlayTimerNotifier, String>((ref) {
+  return PlayTimerNotifier();
+});
+
+class PlayTimerNotifier extends StateNotifier<String> {
+  PlayTimerNotifier() : super("not start");
+
+  setPlay(String newValue) => state = newValue;
 }
 
 class PomodoroScreen extends ConsumerStatefulWidget {
@@ -22,10 +35,16 @@ class PomodoroScreen extends ConsumerStatefulWidget {
 
 class _PomodoroScreenState extends ConsumerState<PomodoroScreen> {
   int duration = 1500;
-  final CountDownController _ctrl = CountDownController();
+  int duration2 = 300;
+  int duration3 = 900;
+  final CountDownController _ctrl1 = CountDownController();
+  final CountDownController _ctrl2 = CountDownController();
+  final CountDownController _ctrl3 = CountDownController();
   @override
   Widget build(BuildContext context) {
+    final timer = ref.watch(playtimerProvider.notifier);
     final int _index = ref.watch(navProvider);
+    final String statusPlay = ref.watch(playtimerProvider);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -35,74 +54,104 @@ class _PomodoroScreenState extends ConsumerState<PomodoroScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 TextButton(
-                    onPressed: () {
-                      ref.watch(navProvider.notifier).setNavigation(0);
-                    },
-                    child: Text("Pomodoro")),
+                  onPressed: () {
+                    ref.watch(navProvider.notifier).setNavigation(0);
+                  },
+                  child: Text(
+                    "Pomodoro",
+                    style: TextStyle(
+                        fontWeight: _index == 0 ? FontWeight.bold : null),
+                  ),
+                ),
                 TextButton(
                     onPressed: () {
                       ref.watch(navProvider.notifier).setNavigation(1);
                     },
-                    child: Text("Break")),
+                    child: Text(
+                      "Break",
+                      style: TextStyle(
+                          fontWeight: _index == 1 ? FontWeight.bold : null),
+                    )),
                 TextButton(
                     onPressed: () {
                       ref.watch(navProvider.notifier).setNavigation(2);
                     },
-                    child: Text("Istirahat")),
+                    child: Text(
+                      "Istirahat",
+                      style: TextStyle(
+                          fontWeight: _index == 2 ? FontWeight.bold : null),
+                    )),
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 40),
-              child: CircularCountDownTimer(
-                duration: 5,
-                initialDuration: 0,
-                controller: _ctrl,
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 2.5,
-                ringColor: Colors.white,
-                fillColor: primary,
-                strokeWidth: 23.0,
-                strokeCap: StrokeCap.round,
-                textStyle: const TextStyle(
-                  // fontFamily: "Digital",
-                  fontSize: 90.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                textFormat: CountdownTextFormat.MM_SS,
-                isReverse: true,
-                isReverseAnimation: true,
-                isTimerTextShown: true,
-                autoStart: false,
-                onStart: () {
-                  debugPrint('Countdown Started');
-                },
-                onComplete: () {
-                  debugPrint('Countdown Ended');
-                  _ctrl.restart();
-                },
-                onChange: (String timeStamp) {
-                  debugPrint('Countdown Changed $timeStamp');
-                },
-                timeFormatterFunction: (defaultFormatterFunction, duration) {
-                  if (duration.inSeconds == 0) {
-                    return "Finish";
-                  } else {
-                    return Function.apply(defaultFormatterFunction, [duration]);
-                  }
-                },
-              ),
+            IndexedStack(
+              index: _index,
+              children: [
+                TimerPomodoro(duration: 5, ctrl1: _ctrl1, ctrl2: _ctrl2),
+                TimerPomodoro(duration: 10, ctrl1: _ctrl2, ctrl2: _ctrl3),
+                TimerPomodoro(duration: 15, ctrl1: _ctrl3, ctrl2: _ctrl1),
+              ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 TextButton(
-                  onPressed: () => _ctrl.reset(),
+                  onPressed: () {
+                    print(_index);
+                    switch (_index) {
+                      case 0:
+                        _ctrl1.reset();
+                        break;
+                      case 1:
+                        _ctrl2.reset();
+                        break;
+                      case 2:
+                        _ctrl3.reset();
+                      default:
+                    }
+                    timer.setPlay("not start");
+                  },
                   child: const Text("Reset"),
                 ),
                 InkWell(
                   onTap: () {
-                    _ctrl.restart();
+                    switch (_index) {
+                      case 0:
+                        if (statusPlay == "not start") {
+                          timer.setPlay("start");
+                          _ctrl1.start();
+                        } else if (statusPlay == "start") {
+                          timer.setPlay("pause");
+                          _ctrl1.pause();
+                        } else {
+                          timer.setPlay("start");
+                          _ctrl1.resume();
+                        }
+                        break;
+                      case 1:
+                        if (statusPlay == "not start") {
+                          timer.setPlay("start");
+                          _ctrl2.start();
+                        } else if (statusPlay == "start") {
+                          timer.setPlay("pause");
+                          _ctrl2.pause();
+                        } else {
+                          timer.setPlay("start");
+                          _ctrl2.resume();
+                        }
+                        break;
+                      case 2:
+                        if (statusPlay == "not start") {
+                          timer.setPlay("start");
+                          _ctrl3.start();
+                        } else if (statusPlay == "start") {
+                          timer.setPlay("pause");
+                          _ctrl3.pause();
+                        } else {
+                          timer.setPlay("start");
+                          _ctrl3.resume();
+                        }
+                      default:
+                    }
                   },
                   child: Container(
                     height: 50,
@@ -112,16 +161,15 @@ class _PomodoroScreenState extends ConsumerState<PomodoroScreen> {
                         border: Border.all(
                           width: 2,
                         )),
-                    child: const Icon(Icons.play_arrow, size: 30),
+                    child: Icon(
+                      statusPlay == "start" ? Icons.pause : Icons.play_arrow,
+                      size: 30,
+                    ),
                   ),
                 ),
                 TextButton(
                     onPressed: () {
-                      setState(() {
-                        duration = 300;
-                      });
-
-                      _ctrl.restart();
+                      _ctrl1.restart();
                     },
                     child: const Text("Lanjut")),
               ],
@@ -129,11 +177,72 @@ class _PomodoroScreenState extends ConsumerState<PomodoroScreen> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _ctrl.restart();
+    );
+  }
+}
+
+class TimerPomodoro extends ConsumerWidget {
+  const TimerPomodoro({
+    super.key,
+    required CountDownController ctrl1,
+    required CountDownController ctrl2,
+    required this.duration,
+  })  : _ctrl1 = ctrl1,
+        _ctrl2 = ctrl2;
+
+  final CountDownController _ctrl1;
+  final CountDownController _ctrl2;
+  final int duration;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40),
+      child: CircularCountDownTimer(
+        duration: duration,
+        initialDuration: 0,
+        controller: _ctrl1,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 2.5,
+        ringColor: Colors.white,
+        fillColor: primary,
+        strokeWidth: 23.0,
+        strokeCap: StrokeCap.round,
+        textStyle: const TextStyle(
+          // fontFamily: "Digital",
+          fontSize: 90.0,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        textFormat: CountdownTextFormat.MM_SS,
+        isReverse: true,
+        isReverseAnimation: true,
+        isTimerTextShown: true,
+        autoStart: false,
+        onStart: () {
+          debugPrint('Countdown Started');
         },
-        child: const Icon(Icons.start),
+        onComplete: () {
+          ref.watch(playtimerProvider.notifier).setPlay('not start');
+          final index = ref.watch(navProvider);
+          print("INDEX PERTASAMA $index");
+          if (index == 2) {
+            ref.watch(navProvider.notifier).setNavigation(0);
+          } else {
+            ref.watch(navProvider.notifier).setNavigation(index + 1);
+          }
+          print("INDEX KEDUA $index");
+        },
+        onChange: (String timeStamp) {
+          debugPrint('Countdown Changed $timeStamp');
+        },
+        timeFormatterFunction: (defaultFormatterFunction, duration) {
+          if (duration.inSeconds == 0) {
+            return "Finish";
+          } else {
+            return Function.apply(defaultFormatterFunction, [duration]);
+          }
+        },
       ),
     );
   }
